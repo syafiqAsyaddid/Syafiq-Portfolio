ee8130c7f18f8fed193b6731750a42c7dd68bad1
syafiqAsyaddid <syafiqasyaddid@gmail.com>
2023-01-12 16:59:52 +0700

    add contact

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ function toggleDetail(e) {
  
     $(detail).slideToggle()
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp-email") 
+    const subject = $("#inp-subject") 
+    const message = $("#inp-message")
+    
+    if(!$(email).val()) {
+        alert("Email is required")
+    } else if(!$(subject).val()) {
+        alert("Subject is required")
+    } else if(!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
